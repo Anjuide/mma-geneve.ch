5fea7339d67b2ca15837ec084e825bb099f4b135
Anjuide <anjuide@users.noreply.github.com>
2019-02-08 17:51:00 +0100

    First

--- index.php
+++ index.php
@@ -40,7 +40,145 @@ include "header.php"
         <span class="sr-only">Suivant</span>
     </a>
 </div>
-
+<!-- DISCIPLINES -->
+<div class="container-fluid">
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-12 col-md-6 col-lg-4 ">
+                <img src="http://planete-mma.com/images/image-slider-2.png" class="img-fluid" alt="Responsive image">
+                <div class="px-2">
+                    <h2 class="titre-discipline">MMA</h2>
+                    <p class="texte-discipline">
+                        Le MMA (Mixed Martial Arts) est un sport de combat intense et évolutif dans lequel les concurents utilisent différents styles de combats, comme le Muay Thai, le BJJ, le Judo, la Boxe et plus encore.<br>
+                        Les règles permettent l'utlisation de techniques de frappe et de grappling à la fois en position debout et au sol.
+                    </p>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+                <img src="http://planete-mma.com/images/image-slider-2.png" class="img-fluid" alt="Responsive image">
+                <div class="px-2">
+                    <h2 class="titre-discipline">BJJ / GRAPPLING</h2>
+                    <p class="texte-discipline">
+                    Dispensé par José Fernandez, certifié Ceinture Violette sous Burton Richardson et Robson (Robinho) Pinheiro, le JJB / Grappling enseigné dans notre académie s'inspire des différents styles existants, 
+                    parmis lesquels nous pouvons citer ceux de : Marcelo Garcia, Eddie Bravo, Pablo Popovitch, ou encore Ryan Hall.
+                    </p>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+                <img src="http://planete-mma.com/images/image-slider-2.png" class="img-fluid" alt="Responsive image">
+                <div class="px-2">
+                    <h2 class="titre-discipline">MUAY THAI</h2>
+                    <p class="texte-discipline">
+                        Les cours sont dispensés par José Fernandez, et quelques aide-instructeurs. la Boxe Thaïlandaise (aussi appelé "Muay Thaï") est une discipline majeure des sports de contact.
+                        Les panoplies de frappes y sont très nombreuses par-rapport à d'autres disciplines (que ce soit le full-contact ou le kick-boxing) ; alliant le pied-poing classique avec les genoux, 
+                        les coudes et le clinch de la nuque, la Boxe Thaïlandaise est une discipline très complète, demandant un très bon cardio.
+                        Sa pratique est essentielle pour les Mixed Martials Arts.
+                    </p>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+                <img src="http://planete-mma.com/images/image-slider-2.png" class="img-fluid" alt="Responsive image">
+                <div class="px-2">
+                    <h2 class="titre-discipline">WRESTLING</h2>
+                    <p class="texte-discipline">
+                        La Lutte (Wrestling) est un système de combat à mains nues au cours duquel les adversaires se mesurent au corps à corps. L'objectif est d'amener à sol votre adversaire au sol à travers différentes techniques.
+                    </p>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+                <img src="http://planete-mma.com/images/image-slider-2.png" class="img-fluid" alt="Responsive image">
+                <div class="px-2">
+                    <h2 class="titre-discipline">BOXE ANGLAISE</h2>
+                    <p class="texte-discipline">
+                        La boxe anglaise est un sport de combat dans lequel deux personnes s'engagent dans un combat utilisant seulement leurs poings. La boxe anglaise est une discipline essentielle pour le MMA, 
+                        car elle vous donne une option offensive et défensive lorsque le combat est debout.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- HORAIRES -->
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                <th scope="col"></th>
+                <th scope="col">Lundi</th>
+                <th scope="col">Mardi</th>
+                <th scope="col">Mercredi</th>
+                <th scope="col">Jeudi</th>
+                <th scope="col">Samedi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Boxe éducative (11-15 ans)</th>
+                    <td>17:30 - 18:30</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">10th Planet Jiu-Jitsu</th>
+                    <td>18:30 - 19:30</td>
+                    <td></td>
+                    <td>18:30 - 19:30</td>
+                    <td>18:30 - 19:30</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">MMA</th>
+                    <td>19:30 - 20:30</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>12:00 - 13:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Cours de boxe pattes d'ours (paos)</th>
+                    <td>20:30 - 21:15</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">Boxe Thai</th>
+                    <td></td>
+                    <td>18:30 - 19:30</td>
+                    <td></td>
+                    <td>19:30 - 20:30</td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">Lutte</th>
+                    <td></td>
+                    <td>19:30 - 20:30</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">10th Planet Jiu-Jitsu (11-15 ans)</th>
+                    <td></td>
+                    <td></td>
+                    <td>17:30 - 18:30</td>
+                    <td></td>
+                    <td>11:00 - 12:00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Boxe anglaise</th>
+                    <td></td>
+                    <td></td>
+                    <td>19:30 - 20:30</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
 <?php
 include "footer.php"
 ?>
