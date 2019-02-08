cbe1e167a8e3117a52763ccb0406cb5e3105973b
Anjuide <ange.gnamba@gmail.com>
2019-02-09 00:14:32 +0100

    Modif disciplines + ajout contact et modif liens images

--- index.php
+++ index.php
@@ -10,21 +10,21 @@ include "header.php"
     </ol>
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="http://planete-mma.com/images/image-slider-1.png" class="d-block w-100" alt="Slide 1">
+            <img src="img/sliders/BJJ_Gi_Level1_Desktop2x_2560x864__1_.jpg" class="d-block w-100" alt="Slide 1">
             <div class="carousel-caption d-none d-md-block">
                 <h5>Where Am I?</h5>
                 <p>I am just on the picture</p>
             </div>
         </div>
         <div class="carousel-item">
-            <img src="http://planete-mma.com/images/image-slider-2.png" class="d-block w-100" alt="Slide 2">
+            <img src="img/sliders/open-uri20151214-3-1fx38j2.jpg" class="d-block w-100" alt="Slide 2">
             <div class="carousel-caption d-none d-md-block">
                 <h5>Where Am I?</h5>
                 <p>I am just on the picture</p>
             </div>
         </div>
         <div class="carousel-item">
-            <img src="http://planete-mma.com/images/image-slider-3.png" class="d-block w-100" alt="Slide 3">
+            <img src="img/sliders/BJJ_Gi_Level2_Desktop2x_2560x864.jpg" class="d-block w-100" alt="Slide 3">
             <div class="carousel-caption d-none d-md-block">
                 <h5>Where Am I?</h5>
                 <p>I am just on the picture</p>
@@ -42,62 +42,149 @@ include "header.php"
 </div>
 <!-- DISCIPLINES -->
 <div class="container-fluid">
-    <div class="container">
-        <div class="row justify-content-md-center">
-            <div class="col-12 col-md-6 col-lg-4 ">
-                <img src="http://planete-mma.com/images/image-slider-2.png" class="img-fluid" alt="Responsive image">
-                <div class="px-2">
-                    <h2 class="titre-discipline">MMA</h2>
-                    <p class="texte-discipline">
-                        Le MMA (Mixed Martial Arts) est un sport de combat intense et évolutif dans lequel les concurents utilisent différents styles de combats, comme le Muay Thai, le BJJ, le Judo, la Boxe et plus encore.<br>
-                        Les règles permettent l'utlisation de techniques de frappe et de grappling à la fois en position debout et au sol.
-                    </p>
+    <div class="container my-5 px-0">
+        <div id="disciplines" class="col bg-black text-white text-left pl-1 pb-3">
+            <h2>Disciplines</h2>
+        </div>
+        <div class="row justify-content-md-center mx-auto">
+            <div id="mma" class="bg-black text-dark col-12 col-md-6 col-lg-4 px-1">
+                <img src="img/disciplines/half_size_image20151214-3-bkl428.jpg" class="img-fluid" alt="Responsive image">
+                <div class="px-0 bg-white">
+                    <div class="px-3 bg-white">
+                        <h2 class="titre-discipline">MMA</h2>
+                        <p class="texte-discipline">
+                            Le MMA (Mixed Martial Arts) est un sport de combat intense et évolutif dans lequel les concurents utilisent différents styles de combats, comme le Muay Thai, le BJJ, le Judo, la Boxe et plus encore.<br>
+                            Les règles permettent l'utlisation de techniques de frappe et de grappling à la fois en position debout et au sol.
+                        </p>
+                    </div>
+                    <div class="px-0 bg-black">
+                        <a class="bouton" href="/classes/boxing-conditioning">
+                            <span class="">
+                                En savoir plus 
+                                <i class=""></i>
+                            </span>
+                        </a>
+                    </div>
                 </div>
             </div>
-            <div class="col-12 col-md-6 col-lg-4">
-                <img src="http://planete-mma.com/images/image-slider-2.png" class="img-fluid" alt="Responsive image">
-                <div class="px-2">
-                    <h2 class="titre-discipline">BJJ / GRAPPLING</h2>
-                    <p class="texte-discipline">
-                    Dispensé par José Fernandez, certifié Ceinture Violette sous Burton Richardson et Robson (Robinho) Pinheiro, le JJB / Grappling enseigné dans notre académie s'inspire des différents styles existants, 
-                    parmis lesquels nous pouvons citer ceux de : Marcelo Garcia, Eddie Bravo, Pablo Popovitch, ou encore Ryan Hall.
-                    </p>
+
+            <div id="bjj" class="bg-black text-dark col-12 col-md-6 col-lg-4 px-1">
+                <img src="img/disciplines/half_size_image20151214-3-1fi8vjr.jpg" class="img-fluid" alt="Responsive image">
+                
+                <div class="px-0 bg-white">
+                    <div class="px-3 bg-white">
+                        <h2 class="titre-discipline">BJJ / GRAPPLING</h2>
+                        <p class="texte-discipline">
+                        Dispensé par José Fernandez, certifié Ceinture Violette sous Burton Richardson et Robson (Robinho) Pinheiro, le JJB / Grappling enseigné dans notre académie s'inspire des différents styles existants, 
+                        parmis lesquels nous pouvons citer ceux de : Marcelo Garcia, Eddie Bravo, Pablo Popovitch, ou encore Ryan Hall.
+                        </p>
+                    </div>
+                    
+                    <div class="px-0 bg-black">
+                        <a class="bouton" href="/classes/boxing-conditioning">
+                            <span class="">
+                                En savoir plus 
+                                <i class=""></i>
+                            </span>
+                        </a>
+                    </div>
+
+                </div>        
+       
+            </div>
+
+            <div id="muaythai"  class="bg-black text-dark col-12 col-md-6 col-lg-4 px-1">
+                <img src="img/disciplines/half_size_image20151214-3-17zo4j1.jpg" class="img-fluid" alt="Responsive image">
+                
+                <div class="px-0 bg-white">
+                    <div class="px-3 bg-white">
+                        <h2 class="titre-discipline">MUAY THAI</h2>
+                        <p class="texte-discipline">
+                            Les cours sont dispensés par José Fernandez, et quelques aide-instructeurs. la Boxe Thaïlandaise (aussi appelé "Muay Thaï") est une discipline majeure des sports de contact.
+                            Les panoplies de frappes y sont très nombreuses par-rapport à d'autres disciplines (que ce soit le full-contact ou le kick-boxing) ; alliant le pied-poing classique avec les genoux, 
+                            les coudes et le clinch de la nuque, la Boxe Thaïlandaise est une discipline très complète, demandant un très bon cardio.
+                            Sa pratique est essentielle pour les Mixed Martials Arts.
+                        </p>
+                    </div>
+                    <div class="px-0 bg-black">
+                            <a class="bouton" href="/classes/boxing-conditioning">
+                                <span class="">
+                                    En savoir plus 
+                                    <i class=""></i>
+                                </span>
+                            </a>
+                    </div>
                 </div>
             </div>
-            <div class="col-12 col-md-6 col-lg-4">
-                <img src="http://planete-mma.com/images/image-slider-2.png" class="img-fluid" alt="Responsive image">
-                <div class="px-2">
-                    <h2 class="titre-discipline">MUAY THAI</h2>
-                    <p class="texte-discipline">
-                        Les cours sont dispensés par José Fernandez, et quelques aide-instructeurs. la Boxe Thaïlandaise (aussi appelé "Muay Thaï") est une discipline majeure des sports de contact.
-                        Les panoplies de frappes y sont très nombreuses par-rapport à d'autres disciplines (que ce soit le full-contact ou le kick-boxing) ; alliant le pied-poing classique avec les genoux, 
-                        les coudes et le clinch de la nuque, la Boxe Thaïlandaise est une discipline très complète, demandant un très bon cardio.
-                        Sa pratique est essentielle pour les Mixed Martials Arts.
-                    </p>
+            <div id="wrestling"  class="bg-black text-dark col-12 col-md-6 col-lg-4 px-1">
+                <img src="img/disciplines/half_size_image20151214-3-13lb43u.jpg" class="img-fluid" alt="Responsive image">
+                
+                <div class="px-0 bg-white">
+                    <div class="px-3 bg-white">
+                        <h2 class="titre-discipline">WRESTLING</h2>
+                        <p class="texte-discipline">
+                            La Lutte (Wrestling) est un système de combat à mains nues au cours duquel les adversaires se mesurent au corps à corps. L'objectif est d'amener à sol votre adversaire au sol à travers différentes techniques.
+                        </p>
+                    </div>
+                    <div class="px-0 bg-black">
+                            <a class="bouton" href="/classes/boxing-conditioning">
+                                <span class="">
+                                    En savoir plus 
+                                    <i class=""></i>
+                                </span>
+                            </a>
+                    </div>
                 </div>
             </div>
-            <div class="col-12 col-md-6 col-lg-4">
-                <img src="http://planete-mma.com/images/image-slider-2.png" class="img-fluid" alt="Responsive image">
-                <div class="px-2">
-                    <h2 class="titre-discipline">WRESTLING</h2>
-                    <p class="texte-discipline">
-                        La Lutte (Wrestling) est un système de combat à mains nues au cours duquel les adversaires se mesurent au corps à corps. L'objectif est d'amener à sol votre adversaire au sol à travers différentes techniques.
-                    </p>
+            <div id="boxeanglaise"  class="bg-black text-dark col-12 col-md-6 col-lg-4 px-1">
+                <img src="img/disciplines/half_size_image20151214-3-19hpi33.jpg" class="img-fluid" alt="Responsive image">
+                
+                <div class="px-0 bg-white">
+                    <div class="px-3 bg-white">
+                        <h2 class="titre-discipline">BOXE ANGLAISE</h2>
+                        <p class="texte-discipline">
+                            La boxe anglaise est un sport de combat dans lequel deux personnes s'engagent dans un combat utilisant seulement leurs poings. La boxe anglaise est une discipline essentielle pour le MMA, 
+                            car elle vous donne une option offensive et défensive lorsque le combat est debout.
+                        </p>
+                    </div>
+                    <div class="px-0 bg-black">
+                            <a class="bouton" href="/classes/boxing-conditioning">
+                                <span class="">
+                                    En savoir plus 
+                                    <i class=""></i>
+                                </span>
+                            </a>
+                    </div>
                 </div>
             </div>
-            <div class="col-12 col-md-6 col-lg-4">
-                <img src="http://planete-mma.com/images/image-slider-2.png" class="img-fluid" alt="Responsive image">
-                <div class="px-2">
-                    <h2 class="titre-discipline">BOXE ANGLAISE</h2>
-                    <p class="texte-discipline">
-                        La boxe anglaise est un sport de combat dans lequel deux personnes s'engagent dans un combat utilisant seulement leurs poings. La boxe anglaise est une discipline essentielle pour le MMA, 
-                        car elle vous donne une option offensive et défensive lorsque le combat est debout.
-                    </p>
+            <div id="leconsprivees"  class="bg-black text-dark col-12 col-md-6 col-lg-4 px-1">
+                <img src="img/disciplines/half_size_image20151214-3-1hptjf.jpg" class="img-fluid" alt="Responsive image">
+                
+                <div class="px-0 bg-white">
+                    <div class="px-3 bg-white">
+                        <h2 class="titre-discipline">LECONS PRIVEES</h2>
+                        <p class="texte-discipline">
+                            La boxe anglaise est un sport de combat dans lequel deux personnes s'engagent dans un combat utilisant seulement leurs poings. La boxe anglaise est une discipline essentielle pour le MMA, 
+                            car elle vous donne une option offensive et défensive lorsque le combat est debout.
+                        </p>
+                    </div>
+                    <div class="px-0 bg-black">
+                            <a class="bouton" href="/classes/boxing-conditioning">
+                                <span class="">
+                                    En savoir plus 
+                                    <i class=""></i>
+                                </span>
+                            </a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-<!-- HORAIRES -->
+    
+    <!-- HORAIRES -->
+    <div id="horaires" class="col bg-black text-white text-left pl-1 pb-3">
+            <h2>Horaires</h2>
+    </div>
     <div class="table-responsive">
         <table class="table table-striped">
             <thead>
@@ -178,7 +265,53 @@ include "header.php"
             </tbody>
         </table>
     </div>
+
+    <!-- CONTACT -->
+    <div class="container">
+        <div id="contact" class="col bg-black text-white text-left pl-1 pb-3">
+            <h2>Contact</h2>
+        </div>
+        <div class="row text-center">
+            <div class="col-12 col-md-4">
+                <i class="fas fa-phone fa-2x"></i>
+                <h4>Phone</h4>
+                <p>+41 78 948 60 29 </p>
+            </div>
+            <div class="col-12 col-md-4">
+                <i class="fas fa-envelope fa-2x"></i>
+                <h4>Email</h4>
+                <p>planetemmage@gmail.com </p>
+            </div>
+            <div class="col-12 col-md-4">
+                <i class="fas fa-map-marker-alt fa-2x"></i>
+                <h4>Adresse</h4>
+                <p>Avenue de Châtelaine 91A, 1219 Châtelaine</p>
+            </div>
+        </div>
+        <form class="my-5">
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputEmail">E-mail</label>
+                    <input type="email" class="form-control" id="inputEmail" placeholder="Entrez votre adresse e-mail" required>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputName">Nom</label>
+                    <input type="text" class="form-control" id="inputName" placeholder="Entrez votre nom" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="inputMessage">Message</label>
+                <textarea rows="4" class="form-control" id="inputMessage" placeholder="Entrez votre message"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Envoyer</button>
+        </form>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2760.9721418804634!2d6.109790115582606!3d46.21100917911683!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sch!4v1520968001608" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+    </div>
+
+
 </div>
+
+<!-- Inclure le footer -->
 <?php
 include "footer.php"
 ?>
